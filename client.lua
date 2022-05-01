f78c429ab735aaf3d58d023f59ececef5716b68e
Mariah Antionette <64466246+mariahantionette@users.noreply.github.com>
2022-05-01 16:35:07 -0500

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,15 @@
+---- Clear Grove Street of Peds 
+Citizen.CreateThread(function()
+	while true do
+	    Citizen.Wait(0)
+        ClearAreaOfPeds( x, y, z,	50,	1)    --Native(x, x, x, Radius, Flags
+	end
+end)
+
+--ClearAreaOfPeds(
+--	x --[[ number ]],
+--	y --[[ number ]], 
+--	z --[[ number ]], 
+--	radius --[[ number ]], 
+--	flags --[[ integer ]]
+--)
