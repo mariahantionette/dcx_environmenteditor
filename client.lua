dd5ece5f6a80cfd4acebbc9fed62896e90c9ef08
Mariah Antionette <64466246+mariahantionette@users.noreply.github.com>
2022-05-02 21:12:01 -0600

    Update client.lua
    Added Bolingbrook Prison

--- client.lua
+++ client.lua
@@ -1,15 +1,15 @@
 ---- Clear Grove Street of Peds 
 Citizen.CreateThread(function()
-	while true do
-	    Citizen.Wait(0)
-        ClearAreaOfPeds( x, y, z,	50,	1)    --Native(x, x, x, Radius, Flags
-	end
+    while (true) do
+	ClearAreaOfPeds(103.88, -1939.43, 20.00, 250.0, 1)
+        Citizen.Wait(0)
+    end
 end)
 
---ClearAreaOfPeds(
---	x --[[ number ]],
---	y --[[ number ]], 
---	z --[[ number ]], 
---	radius --[[ number ]], 
---	flags --[[ integer ]]
---)
+---- Clear Bolingbrook Prison of Peds
+Citizen.CreateThread(function()
+    while (true) do
+	ClearAreaOfPeds(1689.13, 2600.01, 45.56, 300.0, 1)
+        Citizen.Wait(0)
+    end
+end)	
