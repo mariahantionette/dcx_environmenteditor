079f9ba6a8bb270d37d18871f125020e1e7ae469
Mariah Antionette <64466246+mariahantionette@users.noreply.github.com>
2022-05-02 21:42:53 -0600

    Update client.lua
    Added LSMC and also clear area of vehicle functions.

--- client.lua
+++ client.lua
@@ -1,7 +1,14 @@
----- Clear Grove Street of Peds 
+---- Clear Grove Street of Peds & Cars
 Citizen.CreateThread(function()
     while (true) do
 	ClearAreaOfPeds(103.88, -1939.43, 20.00, 250.0, 1)
+        Citizen.Wait(0)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while (true) do
+	ClearAreaOfVehicles(103.88, -1939.43, 20.00, 100.0, false, false, false, false, false)
         Citizen.Wait(0)
     end
 end)
@@ -13,3 +20,19 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
     end
 end)	
+
+---- Clear Los Santos Medical Center
+Citizen.CreateThread(function()
+    while (true) do
+	ClearAreaOfPeds(349.35, -1404.38, 32.51, 85.0, 1)
+        Citizen.Wait(0)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while (true) do
+	ClearAreaOfVehicles(349.35, -1404.38, 32.51, 75.0, false, false, false, false, false)
+        Citizen.Wait(0)
+    end
+end)
+
