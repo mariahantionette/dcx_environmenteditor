83f52170bb7422c76a1133c59addeaa7f04e4c27
Mariah Antionette <64466246+mariahantionette@users.noreply.github.com>
2022-05-02 21:09:04 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DivineChaos#1007'
+description 'Script created to remove peds from a defined area'
+version '1.0.0'
+
+resource_type 'gametype' { name = 'My awesome game type!' }
+
+client_script 'client.lua'
