3e79f3b24de4f1dd61169c4092de0d4d3066c7bc
Mariah Antionette <64466246+mariahantionette@users.noreply.github.com>
2022-05-02 21:56:58 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,16 @@
+Config = {}
 
+Config.Zones = {
+  GroveStreet = {
+    Pos = {
+      vector3(103.88, -1939.43, 20.00) 
+    },
+    Radius = 250
+  },
+  Bolingbrook = {
+    Pos = {
+      vector(1689.13, 2600.01, 45.56)
+  },
+    Radius = 300
+  },
+}
